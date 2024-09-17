b4fefa4a984f5d9cdc1d04c2634e0e39ddb4838a
Roshni Yadav <115661992+roshniyadav12@users.noreply.github.com>
2024-09-18 00:00:45 +0530

    Add files via upload

---
+++ CountNums.java
@@ -0,0 +1,17 @@
+package com.roshni;
+
+public class CountNums {
+    public static void main(String[] args) {
+        int n=24443444;
+        int count=0;
+        while(n>0){
+            int rem= n%10;
+            if(rem==4){
+                count++;
+            }
+            n=n/10;
+        }
+        System.out.println(count);
+
+    }
+}
