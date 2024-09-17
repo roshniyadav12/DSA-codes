e883a15cf5396469b6bdea88ecc6ec42609d17e3
Roshni Yadav <115661992+roshniyadav12@users.noreply.github.com>
2024-09-18 00:08:09 +0530

    Add files via upload

---
+++ FindMin.java
@@ -0,0 +1,11 @@
+package com.roshni;
+
+public class FindMin {
+    public static void main(String[] args) {
+        int[] arr={2,3,4,5,6,7};
+        //System.out.println(min(arr));
+    }
+//    static int min(int[] arr){
+//
+//    }
+}
