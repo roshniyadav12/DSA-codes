2155a00898c9b64d6ed11a47c38946d6e59c8f8c
Roshni Yadav <115661992+roshniyadav12@users.noreply.github.com>
2024-09-17 23:58:00 +0530

    Add files via upload

---
+++ Basics.java
@@ -0,0 +1,24 @@
+package com.roshni;
+
+public class Basics {
+    public static void main(String[] args) {
+        // if(true){
+        //     System.out.println("honey");
+        // }
+
+        // int a=10;
+        // if(a==10){
+        //     System.out.println("honey");
+        // }
+
+        // int count= 1;
+        // while(count != 5){
+        //     System.out.println(count);
+        //     count++;
+        // }
+
+        for(int count=1; count != 5; count++){
+            System.out.println(count);
+        }
+    }
+}
