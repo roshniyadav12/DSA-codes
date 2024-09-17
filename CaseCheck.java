2343fd4672bcfbab5e3b5465d80700bf29425fb4
Roshni Yadav <115661992+roshniyadav12@users.noreply.github.com>
2024-09-17 23:59:52 +0530

    Add files via upload

---
+++ CaseCheck.java
@@ -0,0 +1,22 @@
+package com.roshni;
+
+import java.util.Scanner;
+
+public class CaseCheck {
+    public static void main(String[] args) {
+        // String word= "Honey";
+        // System.out.println(word.charAt(0));
+
+        Scanner sc= new Scanner(System.in);
+        char ch = sc.next().trim().charAt(0);
+        if(ch>='a' && ch<='z'){
+            System.out.println("Lowercase");
+        }
+        else{
+            System.out.println("Uppercase");
+        }
+
+
+        System.out.println(ch);
+    }
+}
